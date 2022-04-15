660bee17683ea4f39d809e6e774e2776f18f1286
影子你陪着我累吗 <1477649017@qq.com>
2022-04-15 23:12:02 +0800

    test-4-15 冒泡排序优化

---
+++ test-4-15.c
@@ -0,0 +1,31 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main() {
+//	int arr[5] = { 5,2,3,4,6 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (int i = 0;i < sz - 1;i++) {
+//		int flag = 1;//假设是有序的
+//		for (int j = 0;j < sz - i - 1;j++) {
+//			if (arr[j] > arr[j + 1]) {
+//				int tmp = 0;
+//				tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//				flag = 0;
+//			}
+//		}
+//		if (flag == 1) {
+//			break;
+//		}
+//	}
+//	for (int i = 0;i < sz;i++) {
+//		printf("%d ", arr[i]);
+//	}
+//
+//}
+
+int main() {
+	signed char ch = -24;
+	printf("%d\n", ch);
+	return 0;
+}
